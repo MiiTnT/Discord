5e17bb62e7ce39369a0df32502be2b0bac2b5437
Discord <prevert240806@gmail.com>
2020-10-27 14:06:08 +0100

    Patch 0.1

--- src/main/java/Discord/commands/discordCMD.java
+++ src/main/java/Discord/commands/discordCMD.java
@@ -21,33 +21,42 @@ public class discordCMD implements CommandExecutor {
         }else {
             if (args[0].equalsIgnoreCase("help")) {
                 sender.sendMessage("§7=-=-=-=-=-=§c[Discord]§7=-=-=-=-=-=");
-                sender.sendMessage("§e/discord §7Obtenir le lien du discord");
+                sender.sendMessage("§e/Discord §7Obtenir le lien du discord.");
                 if (sender.hasPermission("discord.admin") || sender.isOp()) {
                     sender.sendMessage("§7=-=-=-=-=-=§c[ADMIN]§7=-=-=-=-=-=");
-                    sender.sendMessage("§e/Discord link <link> §7Configurer le lien vers le discord");
+                    sender.sendMessage("§e/Discord link <link> §7Configurer le lien vers le discord.");
+                    sender.sendMessage("§e/Discord reload §7Reload la configuration du plugin.");
                 }
                 sender.sendMessage("§7=-=-=-=-=-=§c[Discord]§7=-=-=-=-=-=");
             }
             if (args[0].equalsIgnoreCase("link")) {
-                try {
-                    if(args[1].length() >= 1) {
-                        if(args[1].contains("https://") || args[1].contains("http://")) {
-                            sender.sendMessage("§c[Discord] §eNouveau lien: §a" + args[1]);
-                            this.main.getConfig().set("DiscordLink", args[1]);
-                            this.main.saveConfig();
-                            this.main.reloadConfig();
-                        }else{
-                            sender.sendMessage("§c[Discord] §cErreur: §7Veuilez indiquer un lien !");
-                        }
+                if(sender.hasPermission("discord.link") || sender.isOp()) {
+                    try {
+                        if (args[1].length() >= 1) {
+                            if (args[1].contains("https://") || args[1].contains("http://")) {
+                                sender.sendMessage("§c[Discord] §eNouveau lien: §a" + args[1]);
+                                this.main.getConfig().set("DiscordLink", args[1]);
+                                this.main.saveConfig();
+                                this.main.reloadConfig();
+                            } else {
+                                sender.sendMessage("§c[Discord] §cErreur: §7Veuilez indiquer un lien !");
+                            }
 
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        sender.sendMessage("§c[Discord] §cErreur: §7Veuilez indiquer un lien !");
                     }
-                }catch(ArrayIndexOutOfBoundsException e){
-                    sender.sendMessage("§c[Discord] §cErreur: §7Veuilez indiquer un lien !");
+                }else{
+                    sender.sendMessage("§c[Discord] §cErreur: §7Vous n'avez pas la permission !");
                 }
             }
             if (args[0].equalsIgnoreCase("reload")) {
-                this.main.reloadConfig();
-                sender.sendMessage("§c[Discord] §eConfiguration rechargée avec succès !");
+                if(sender.hasPermission("discord.reload") || sender.isOp()) {
+                    this.main.reloadConfig();
+                    sender.sendMessage("§c[Discord] §eConfiguration rechargée avec succès !");
+                }else{
+                    sender.sendMessage("§c[Discord] §cErreur: §7Vous n'avez pas la permission !");
+                }
             }
         }
         return false;
